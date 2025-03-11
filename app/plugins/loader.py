f9ec1ab6c1ea17906773d08d427bb34fb0c62ea2
amilmshaji <msamilmanadiyil2560@gmail.com>
2025-03-11 13:29:01 +0530

    save

--- app/plugins/loader.py
+++ app/plugins/loader.py
@@ -7,7 +7,7 @@ from app.plugins.airtable.handler import Airtable
 from app.plugins.website.handler import Website
 from app.plugins.document.handler import Document
 from app.plugins.sqlite.handler import Sqlite
-from app.plugins.maria.handler import Maria
+# from app.plugins.maria.handler import Maria
 from loguru import logger
 
 
@@ -26,7 +26,7 @@ class DSLoader:
             "document" : Document,
             "sqlite" : Sqlite,
             "CSV" : CSVPlugin,
-            "maria": Maria,
+            # "maria": Maria,
         }
         db_type = self.config.get("type","")
         connection_params = self.config.get("params",{})
